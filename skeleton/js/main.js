bf257cc8b43d53546262c54b503d7c6e6063dd24
App Academy Student <student@appacademy.io>
2016-04-13 10:39:49 -0700

    built out grid and basic styling

--- skeleton/js/main.js
+++ skeleton/js/main.js
@@ -3,6 +3,7 @@ var Game = require('../../ttt-core-solution/game');
 
 $(function () {
   // Your code here
+  var game = new Game();
+  var view = new View(game, $(".ttt"));
+  view.setupBoard();
 });
-
-
--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -1,4 +1,6 @@
 var View = function (game, $el) {
+  this.game = game;
+  this.$el = $el;
 };
 
 View.prototype.bindEvents = function () {
@@ -8,6 +10,19 @@ View.prototype.makeMove = function ($square) {
 };
 
 View.prototype.setupBoard = function () {
+  var $grid = $("<ul></ul>");
+  $grid.addClass("grid");
+  $grid.addClass("group");
+  this.$el.append($grid);
+
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      var $listItem = $("<li></li>");
+      $listItem.attr("data-pos", [i,j]);
+      $listItem.addClass("square");
+      $grid.append($listItem);
+    }
+  }
 };
 
 module.exports = View;
